b81fb9facdfec25f5dc097b9afbea4915ac15746
DenisShepel <deniseolanta@gmail.com>
2022-08-16 20:21:50 +0300

    Lesson MY 5

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-a=12
-b=2
-c=3
-#-
-f=a-b-c
-print(f)
+a=1#int
+b=1.25#float
+c="Hello"#str
+d=False#bool
+f=True#bool
+a1=1988#int
+b1=2.25 #float
+print(type(b1))
+c1="2809Denis1988"#str
+print(type(c1))
+f1=1<2 #bool
+print(type(f1))
+h=1>2#bool
+print(type(h))
+name=input("Введите имя:Денис")
+print("Привет",name)
+
+
+
+
+
