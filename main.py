1eef3cf276448c3825080b887bf39cab0d914872
DenisShepel <deniseolanta@gmail.com>
2022-08-20 21:18:31 +0300

    Уравнения

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-a=1#int
-b=1.25#float
-c="Hello"#str
-d=False#bool
-f=True#bool
-a1=1988#int
-b1=2.25 #float
-print(type(b1))
-c1="2809Denis1988"#str
-print(type(c1))
-f1=1<2 #bool
-print(type(f1))
-h=1>2#bool
-print(type(h))
-name=input("Введите имя:Денис")
-print("Привет",name)
+import math
+
+x=int(input("введите число"))
+y=int(input("введите число"))
+c=math.pow(x,y) #ввозведение в степень
+print(c)
+a=int(input("введите число"))
+f=math.sqrt(a) #узнать корень
+print(f)
+a1=int(input("введите число"))
+b1=int(input("введите число"))
+c1=int(input("введите число"))
+x=a1+b1*c1
+print(x)
+
+
+
+
+
+
+
+
+
+
 
 
 
